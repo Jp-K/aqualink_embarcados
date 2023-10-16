0e1a16f194ffc4666098b371e07898b2ee07609b
Jp-K <joaopaulokraisch@gmail.com>
2023-10-15 23:06:33 -0300

    cors

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -9,6 +10,17 @@ app.mount('/static', StaticFiles(directory='static', html=True), name='static')
 
 templates = Jinja2Templates(directory="templates")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get('/')
+def index():
+    return 'See /{id}'
 
 @app.get("/{id}", response_class=HTMLResponse)
 async def read_item(request: Request, id: str):
