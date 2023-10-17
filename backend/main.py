ee4bd75d2382793ce77dd3877603f6ec67124487
Jp-K <joaopaulokraisch@gmail.com>
2023-10-16 21:43:53 -0300

    .

--- backend/main.py
+++ backend/main.py
@@ -25,4 +25,4 @@ def index():
 @app.get("/{id}", response_class=HTMLResponse)
 async def read_item(request: Request, id: str):
 
-    return templates.TemplateResponse("./page.html", {"request": request, "id": id})
+    return templates.TemplateResponse("./page.html", {"request": request, "tempAnterior": id})
