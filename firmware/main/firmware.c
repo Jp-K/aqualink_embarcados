cc768fa19660c4d582c61dd751fddd0b6bacc712
joaohutner <joaovictorhutner@hotmail.com>
2023-10-24 21:42:11 -0300

    att-server-communication

--- firmware/main/firmware.c
+++ firmware/main/firmware.c
@@ -18,57 +18,52 @@
 //Biblioteca para o DHT11
 #include "include/dht11.h"
 
-
-#define LED_RES_PIN 2 //da esp
-#define DHT_PIN 4  //azul
-#define RES_PIN 25 //marrom claro
-#define LED_PIN 26 //marrom escuro
-
-#define LDR_PIN 34 //ADC1_6 ADC1_CHANNEL_6 //laranja
+#define LED_RES_PIN 2 //LED da ESP
+#define DHT_PIN 4  //CABO AZUL
+#define RES_PIN 25 //CABO MARROM CLARO
+#define LED_PIN 26 //CABO MARROM ESCURO
+#define LDR_PIN 34 //CABO LARANJA // ADC1_6 ADC1_CHANNEL_6
 
 static const char *TAGwifi = "wifi_task";
 static const char *TAGr = "reading_task";
 static const char *TAGs = "server_task";
 
+int ldr_value = 0;
+int temperature = 0;
+
 
 void getReadingsTask(void * parameters){
     //Var Init
-    uint32_t voltage;
-    int reading = 0;
-    int led_res_level = 0;
-    int led_level = 0;
-    int reading_delay = 1000;
+    int reading_delay = 500;
+    int status = -1;
+    //uint32_t voltage = 0;
 
     //DHT Setup
     DHT11_init(DHT_PIN);
 
     //LDR Setup
-    esp_adc_cal_characteristics_t adc1_chars; //Calibracao LDR
-    adc1_config_channel_atten(ADC1_CHANNEL_6,ADC_ATTEN_DB_0); //Atenuacao LDR
-    adc1_config_width(ADC_WIDTH_BIT_DEFAULT); //Widht LDR
-    esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_0, ADC_WIDTH_BIT_DEFAULT, 0, &adc1_chars); //Calibracao LDR
+    esp_adc_cal_characteristics_t adc1_chars;
+    adc1_config_channel_atten(ADC1_CHANNEL_6,ADC_ATTEN_DB_6); //Atenuacao LDR
+    adc1_config_width(ADC_WIDTH_BIT_12); //Widht LDR
+    esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_6, ADC_WIDTH_BIT_12, 0, &adc1_chars); //Calibracao LDR
 
     //Laço
     for( ;; ){
         ESP_LOGI(TAGr,"Get sensors readings");
 
         //LDR
-        reading = adc1_get_raw(ADC1_CHANNEL_6);
-        voltage = esp_adc_cal_raw_to_voltage(reading, &adc1_chars);
-        ESP_LOGI(TAGr, "LDR: %d", reading);
-        ESP_LOGI(TAGr, "LDR_ADC: %lu mV", voltage);
+        ldr_value = 4095 - adc1_get_raw(ADC1_CHANNEL_6);
+        //voltage = esp_adc_cal_raw_to_voltage(reading, &adc1_chars);
+        ESP_LOGI(TAGr, "LDR: %d", ldr_value);
+        //ESP_LOGI(TAGr, "LDR_ADC: %lu mV", voltage);
 
         //DHT11
-        ESP_LOGI(TAGr,"Temperature is %d", DHT11_read().temperature);
-        ESP_LOGI(TAGr,"Humidity is %d", DHT11_read().humidity);
-        ESP_LOGI(TAGr,"Status code is %d", DHT11_read().status);
-
-        //Leds
-        led_res_level = ~led_res_level;
-        led_level = ~led_level;
-        gpio_set_level(LED_RES_PIN, led_res_level);
-        gpio_set_level(LED_PIN, led_level);
-
+        temperature = DHT11_read().temperature;
+        status = DHT11_read().status;
+        ESP_LOGI(TAGr,"Status code is %d", status);
+        ESP_LOGI(TAGr,"Temperature is %d", temperature);
+        //ESP_LOGI(TAGr,"Humidity is %d", DHT11_read().humidity);
+        
         //Delay
         ESP_LOGI(TAGr,"Sensors reading finished. Next in: %d\n", reading_delay);
         vTaskDelay(reading_delay);
@@ -86,7 +81,7 @@ void app_main(void)
     ESP_ERROR_CHECK(ret);
 
     ESP_LOGI(TAGwifi, "Starting Wifi Login ... ... \n"); //Utilizar menuconfig para configurar rede
-    //wifi_init_sta();
+    wifi_init_sta();
     ESP_LOGI(TAGwifi, "Wifi is Running! \n");
 
     // GPIO initialization POT + LED INBOARD
@@ -108,7 +103,7 @@ void app_main(void)
     gpio_set_direction(DHT_PIN, GPIO_MODE_INPUT);
 
     ESP_LOGI(TAGs, "Starting Web Server ... ...\n");
-    //setup_server();
+    setup_server(&temperature, &ldr_value);
     ESP_LOGI(TAGs, "Web Server is running! \n");
 
    // xTaskCreate(&serverTask, "serverTask",2048,NULL,5,NULL); //funct_name, task_name, stack_size, task_param, task_priority, task_handle
--- firmware/main/server.c
+++ firmware/main/server.c
@@ -1,32 +1,43 @@
 #include "include/server.h"
 
-#define LED_RES_PIN 2 //da esp
-#define DHT_PIN 4  //azul
-#define RES_PIN 25 //marrom claro
-#define LED_PIN 26 //marrom escuro
-
-#define LDR_PIN 34 //ADC1_6 ADC1_CHANNEL_6 //laranja
-
-char on_resp[] = "<!DOCTYPE html><html><head><style type=\"text/css\">html {  font-family: Arial;  display: inline-block;  margin: 0px auto;  text-align: center;}h1{  color: #070812;  padding: 2vh;}.button {  display: inline-block;  background-color: #b30000; //red color  border: none;  border-radius: 4px;  color: white;  padding: 16px 40px;  text-decoration: none;  font-size: 30px;  margin: 2px;  cursor: pointer;}.button2 {  background-color: #364cf4; //blue color}.content {   padding: 50px;}.card-grid {  max-width: 800px;  margin: 0 auto;  display: grid;  grid-gap: 2rem;  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));}.card {  background-color: white;  box-shadow: 2px 2px 12px 1px rgba(140,140,140,.5);}.card-title {  font-size: 1.2rem;  font-weight: bold;  color: #034078}</style>  <title>ESP32 WEB SERVER</title>  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">  <link rel=\"icon\" href=\"data:,\">  <link rel=\"stylesheet\" href=\"https://use.fontawesome.com/releases/v5.7.2/css/all.css\"    integrity=\"sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr\" crossorigin=\"anonymous\">  <link rel=\"stylesheet\" type=\"text/css\" ></head><body>  <h2>ESP32 WEB SERVER</h2>  <div class=\"content\">    <div class=\"card-grid\">      <div class=\"card\">        <p><i class=\"fas fa-lightbulb fa-2x\" style=\"color:#c81919;\"></i>     <strong>GPIO2</strong></p>        <p>GPIO state: <strong> ON</strong></p>        <p>          <a href=\"/led2on\"><button class=\"button\">ON</button></a>          <a href=\"/led2off\"><button class=\"button button2\">OFF</button></a>        </p>      </div>    </div>  </div></body></html>";
-
-char off_resp[] = "<!DOCTYPE html><html><head><style type=\"text/css\">html {  font-family: Arial;  display: inline-block;  margin: 0px auto;  text-align: center;}h1{  color: #070812;  padding: 2vh;}.button {  display: inline-block;  background-color: #b30000; //red color  border: none;  border-radius: 4px;  color: white;  padding: 16px 40px;  text-decoration: none;  font-size: 30px;  margin: 2px;  cursor: pointer;}.button2 {  background-color: #364cf4; //blue color}.content {   padding: 50px;}.card-grid {  max-width: 800px;  margin: 0 auto;  display: grid;  grid-gap: 2rem;  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));}.card {  background-color: white;  box-shadow: 2px 2px 12px 1px rgba(140,140,140,.5);}.card-title {  font-size: 1.2rem;  font-weight: bold;  color: #034078}</style>  <title>ESP32 WEB SERVER</title>  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">  <link rel=\"icon\" href=\"data:,\">  <link rel=\"stylesheet\" href=\"https://use.fontawesome.com/releases/v5.7.2/css/all.css\"    integrity=\"sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr\" crossorigin=\"anonymous\">  <link rel=\"stylesheet\" type=\"text/css\"></head><body>  <h2>ESP32 WEB SERVER</h2>  <div class=\"content\">    <div class=\"card-grid\">      <div class=\"card\">        <p><i class=\"fas fa-lightbulb fa-2x\" style=\"color:#c81919;\"></i>     <strong>GPIO2</strong></p>        <p>GPIO state: <strong> OFF</strong></p>        <p>          <a href=\"/led2on\"><button class=\"button\">ON</button></a>          <a href=\"/led2off\"><button class=\"button button2\">OFF</button></a>        </p>      </div>    </div>  </div></body></html>";
-
-//char on_resp[] = "on";
-
-//char off_resp[] = "off";
+#define LED_RES_PIN 2 //LED da ESP
+#define DHT_PIN 4  //CABO AZUL
+#define RES_PIN 25 //CABO MARROM CLARO
+#define LED_PIN 26 //CABO MARROM ESCURO
+#define LDR_PIN 34 //CABO LARANJA // ADC1_6 ADC1_CHANNEL_6
 
 
 static const char *TAG = "server"; // TAG for debug
-int led_pot_state = 0;
+int res_state = 0;
+int led_state = 0;
+int *temp = 0; //endereço da memoria passado
+int *lum = 0; //endereço da memoria passado
 
 
 esp_err_t send_web_page(httpd_req_t *req)
 {
     int response;
-    if (led_pot_state == 0)
-        response = httpd_resp_send(req, off_resp, HTTPD_RESP_USE_STRLEN);
-    else
-        response = httpd_resp_send(req, on_resp, HTTPD_RESP_USE_STRLEN);
+    char default_resp[100] = "";
+    char resp_string_temp[10];
+    char resp_string_lum[10];
+    char resp_string_led[10];
+    char resp_string_res[10];
+
+
+    sprintf(resp_string_temp, "%d", (int)*temp);   //temperatura para string
+    sprintf(resp_string_lum, "%d", *lum);               //luminosidade para string
+    sprintf(resp_string_led, "%d", led_state);     // estado do led para string
+    sprintf(resp_string_res, "%d", res_state);     // estado do resistor para string
+
+    strcat(default_resp, resp_string_temp);
+    strcat(default_resp, ",");
+    strcat(default_resp, resp_string_lum);
+    strcat(default_resp, ",");
+    strcat(default_resp, resp_string_res);
+    strcat(default_resp, ",");
+    strcat(default_resp, resp_string_led);
+    
+    response = httpd_resp_send(req,default_resp,HTTPD_RESP_USE_STRLEN);
     return response;
 }
 
@@ -35,51 +46,83 @@ esp_err_t get_req_handler(httpd_req_t *req)
     return send_web_page(req);
 }
 
-esp_err_t led_on_handler(httpd_req_t *req)
+esp_err_t res_on_handler(httpd_req_t *req)
 {
     gpio_set_level(LED_RES_PIN, 1);
     gpio_set_level(RES_PIN, 1);
-    led_pot_state = 1;
+    res_state = 1;
+    return send_web_page(req);
+}
+
+esp_err_t res_off_handler(httpd_req_t *req)
+{
+    gpio_set_level(LED_RES_PIN, 0);
+    gpio_set_level(RES_PIN, 0);
+    res_state = 0;
+    return send_web_page(req);
+}
+
+esp_err_t led_on_handler(httpd_req_t *req)
+{
+    gpio_set_level(LED_PIN, 1);
+    led_state = 1;
     return send_web_page(req);
 }
 
 esp_err_t led_off_handler(httpd_req_t *req)
 {
-    gpio_set_level(LED_RES_PIN, 0);
-    gpio_set_level(RES_PIN, 0);
-    led_pot_state = 0;
+    gpio_set_level(LED_PIN, 0);
+    led_state = 0;
     return send_web_page(req);
 }
 
+
 httpd_uri_t uri_get = {
-    .uri = "/",
+    .uri = "/get",
     .method = HTTP_GET,
     .handler = get_req_handler,
     .user_ctx = NULL};
 
-httpd_uri_t uri_on = {
+httpd_uri_t uri_led_on = {
     .uri = "/led2on",
     .method = HTTP_GET,
     .handler = led_on_handler,
     .user_ctx = NULL};
 
-httpd_uri_t uri_off = {
+httpd_uri_t uri_led_off = {
     .uri = "/led2off",
     .method = HTTP_GET,
     .handler = led_off_handler,
     .user_ctx = NULL};
 
-httpd_handle_t setup_server(void)
+httpd_uri_t uri_res_on = {
+    .uri = "/res2on",
+    .method = HTTP_GET,
+    .handler = res_on_handler,
+    .user_ctx = NULL};
+
+httpd_uri_t uri_res_off = {
+    .uri = "/res2off",
+    .method = HTTP_GET,
+    .handler = res_off_handler,
+    .user_ctx = NULL};
+
+httpd_handle_t setup_server(int *temp_ref, int *lum_ref)
 {
     //led_pot_state = 0;
+    temp = temp_ref;
+    lum = lum_ref;
+
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
     httpd_handle_t server = NULL;
 
     if (httpd_start(&server, &config) == ESP_OK)
     {
         httpd_register_uri_handler(server, &uri_get);
-        httpd_register_uri_handler(server, &uri_on);
-        httpd_register_uri_handler(server, &uri_off);
+        httpd_register_uri_handler(server, &uri_res_on);
+        httpd_register_uri_handler(server, &uri_res_off);
+        httpd_register_uri_handler(server, &uri_led_on);
+        httpd_register_uri_handler(server, &uri_led_off);
     }
     return server;
 }
